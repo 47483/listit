a53c6f2e3780604c2111f61f1bc1657e1bf88243
47483 <47483@gymnasium.ax>
2023-05-15 15:37:18 +0300

    Added API Description

--- API/index.php
+++ API/index.php
@@ -4,7 +4,18 @@ require_once("restricted/listfunctions.php");
 require_once("restricted/itemfunctions.php");
 
 $uri = filter_var($_SERVER['REQUEST_URI'], FILTER_UNSAFE_RAW);
+$uriParts = explode("/",$uri);
+$uriParts = array_filter($uriParts);
 
-header("Content-Type:application/json; charset=UTF-8");
-echo json_encode(login(), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+switch (end($uriParts)) {
+    case "login":
+        $return = login();
+}
+
+if (isset($return)) {
+    header("Content-Type:application/json; charset=UTF-8");
+    echo json_encode(login(), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+} else {
+    readfile("info.html");
+}
 ?>
