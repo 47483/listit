efe2d468cc95e27595d1cdf5b015d0d7e0bcfddf
47483 <47483@gymnasium.ax>
2023-05-17 10:20:09 +0300

    Completed Page Auth

--- js/login.js
+++ js/login.js
@@ -37,3 +37,32 @@ function loginsignup(method) {
         })
     }
 }
+
+function auth() {
+    fd = new FormData;
+    fd.append("email",localStorage.getItem("listitEmail"));
+    fd.append("password",localStorage.getItem("listitPassword"));
+    
+    fetch("API/login",{
+        method:"POST",
+        body:fd
+    })
+    
+    .then(response=>{
+        if (response.status == 200) {
+            return response.json();
+        }
+    })
+    
+    .then(data=>{
+        if (data.result) {
+            window.location.replace("listit.html");
+
+        } else {
+            localStorage.removeItem("listitEmail");
+            localStorage.removeItem("listitPassword");
+        }
+    })
+}
+
+auth();
