f8ae4a6d5cd9ea53a4c5fe6b37a8fe720cb47730
47483 <47483@gymnasium.ax>
2023-05-25 08:40:46 +0300

    Added The Rest Of The Comments To API

--- API/restricted/universalfunctions.php
+++ API/restricted/universalfunctions.php
@@ -1,5 +1,7 @@
 <?php
+//A function for connecting to the db
 function db():PDO {
+    //Use parameters below and return the connection
     $dsn = 'mysql:dbname=listit_db;host=localhost';
     $dbUser='dbAccess';
     $dbPassword="letMeIn";
@@ -8,10 +10,12 @@ function db():PDO {
     return $db;
 }
 
+//A function for getting the users information
 function userinfo() {
     $email = null;
     $password = null;
 
+    //Check for provided email
     if (isset($_POST["email"])) {
         $email = filter_var($_POST["email"], FILTER_SANITIZE_SPECIAL_CHARS);
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -19,15 +23,19 @@ function userinfo() {
         }
     }
 
+    //Check for provided password
     if (isset($_POST["password"])) {
         $password = filter_var($_POST["password"], FILTER_SANITIZE_SPECIAL_CHARS);
         $password = hash("sha1", $password);
     }
 
+    //Return userinfo
     return [$email, $password];
 }
 
+//A function for creating responses
 function respond($msg, $result) {
+    //Create a JSON object using params and return
     $response = new stdClass();
     $response->message = $msg;
     $response->result = $result;
--- API/restricted/userfunctions.php
+++ API/restricted/userfunctions.php
@@ -1,9 +1,13 @@
 <?php
+//Require necessary functions
 require_once("universalfunctions.php");
 
+//A function for logging in the user
 function login($db) {
+    //Get user email and password
     $userinfo = userinfo();
 
+    //Make sure all API-inputs are set, else return negative
     if ($userinfo[0] == null) {
         return respond("No email provided.",false);
 
@@ -11,6 +15,7 @@ function login($db) {
         return respond("No password provided.",false);
 
     } else {
+        //Try to log in user using provided email and password, return accordingly
         $stmt=$db->prepare('SELECT `password` FROM `users` WHERE `email`=:email');
         $stmt->execute(["email"=>$userinfo[0]]);
         if ($row=$stmt->fetch()) {
@@ -27,9 +32,12 @@ function login($db) {
     }
 }
 
+//A function for creating a new user
 function signup($db) {
+    //Get user email and password
     $userinfo = userinfo();
 
+    //Make sure all API-inputs are set, else return negative
     if ($userinfo[0] == null) {
         return respond("Invalid email provided.",false);
 
@@ -37,12 +45,14 @@ function signup($db) {
         return respond("No password provided.",false);
     
     } else {
+        //Check if provided email already exists, return negative if it does
         $stmt=$db->prepare('SELECT `email` FROM `users` WHERE `email`=:email');
         $stmt->execute(["email"=>$userinfo[0]]);
         if ($row=$stmt->fetch()) {
             return respond("Email already taken.",false);
 
         } else {
+            //Add new user using provided params, return accordingly
             $stmt=$db->prepare('INSERT INTO `users` (`email`,`password`) VALUES (:email,:password)');
             if ($stmt->execute(["email"=>$userinfo[0],"password"=>$userinfo[1]])) {
                 return respond("Account created successfully.",true);
